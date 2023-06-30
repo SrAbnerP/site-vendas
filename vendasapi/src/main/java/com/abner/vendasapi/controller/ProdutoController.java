070dfb1a0b84ace66adab8ee97a02c27f6d8dd4a
SrAbnerP <abner.sol.pereira@gmail.com>
2023-06-30 18:02:27 -0300

    att

--- vendasapi/src/main/java/com/abner/vendasapi/controller/ProdutoController.java
+++ vendasapi/src/main/java/com/abner/vendasapi/controller/ProdutoController.java
@@ -1,8 +1,13 @@
 package com.abner.vendasapi.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +29,21 @@ public class ProdutoController {
 		Produto produto = dto.toModel();
 		produtoRepository.save(produto);
 		return ProdutoDTO.fromModel(produto);
+	}
+
+	@PutMapping("{id}")
+	public ResponseEntity<Void> atualizar(@PathVariable("id") Long id, @RequestBody ProdutoDTO dto) {
+		Optional<Produto> p = produtoRepository.findById(id);
+
+		if (p.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Produto produto = dto.toModel();
+		produto.setId(id);
+		produtoRepository.save(produto);
+
+		return ResponseEntity.ok().build();
 	}
 
 }
