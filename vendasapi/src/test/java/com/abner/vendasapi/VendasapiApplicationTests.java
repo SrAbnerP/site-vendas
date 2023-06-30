91315c4fea69131ddf36cb2d64a202c44869422c
SrAbnerP <abner.sol.pereira@gmail.com>
2023-06-30 14:47:46 -0300

    criando o back end

---
+++ vendasapi/src/test/java/com/abner/vendasapi/VendasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abner.vendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
