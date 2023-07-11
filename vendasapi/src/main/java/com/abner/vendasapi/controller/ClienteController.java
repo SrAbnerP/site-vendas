2ec7594138d5a6fd7f92a7cb2083f40fdd5cdf67
SrAbnerP <abner.sol.pereira@gmail.com>
2023-07-11 19:19:10 -0300

    add metodo de pegar id, delete e pegar uma lista de clientes

--- vendasapi/src/main/java/com/abner/vendasapi/controller/ClienteController.java
+++ vendasapi/src/main/java/com/abner/vendasapi/controller/ClienteController.java
@@ -1,10 +1,14 @@
 package com.abner.vendasapi.controller;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -44,5 +48,27 @@ public class ClienteController {
 		cliente.setId(id);
 		clienteRepository.save(cliente);
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<ClienteDTO> getById(@PathVariable("id") Long id) {
+
+		return clienteRepository.findById(id).map(cliente -> ClienteDTO.fromModel(cliente))
+				.map(clienteDTO -> ResponseEntity.ok(clienteDTO)).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
+
+		return clienteRepository.findById(id).map(cliente -> {
+			clienteRepository.delete(cliente);
+			return ResponseEntity.noContent().build();
+		}).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@GetMapping
+	public List<ClienteDTO> getLista() {
+		return clienteRepository.findAll().stream().map(clienteDTO -> ClienteDTO.fromModel(clienteDTO))
+				.collect(Collectors.toList());
 	}
 }
