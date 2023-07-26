f4425818ef6acdcfb44da66f8bfc826cfe49db27
SrAbnerP <abner.sol.pereira@gmail.com>
2023-07-26 20:33:17 -0300

    add itemVenda e add o metodo de salvar vendas

--- vendasapi/src/main/java/com/abner/vendasapi/controller/VendaController.java
+++ vendasapi/src/main/java/com/abner/vendasapi/controller/VendaController.java
@@ -1,12 +1,15 @@
 package com.abner.vendasapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abner.vendasapi.model.Venda;
+import com.abner.vendasapi.model.repository.ItemVendaRepository;
 import com.abner.vendasapi.model.repository.VendaRepository;
 
 @RestController
@@ -17,8 +20,16 @@ public class VendaController {
 	@Autowired
 	private VendaRepository vendaRepository;
 
-	public void realizarVenda(@RequestBody Venda venda) {
+	@Autowired
+	private ItemVendaRepository itemVendaRepository;
 
+	@PostMapping
+	@Transactional
+	public void realizarVenda(@RequestBody Venda venda) {
+		vendaRepository.save(venda);
+
+		venda.getItens().stream().forEach(iv -> iv.setVenda(venda));
+		itemVendaRepository.saveAll(venda.getItens());
 	}
 
 }
---
+++ vendasapi/src/main/java/com/abner/vendasapi/model/repository/ItemVendaRepository.java
@@ -0,0 +1,9 @@
+package com.abner.vendasapi.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abner.vendasapi.model.ItemVenda;
+
+public interface ItemVendaRepository extends JpaRepository<ItemVenda, Long> {
+
+}
